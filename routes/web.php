9f4167961bf972e5d3090b584a6f6e787a59bc87
Lizzandro <holanda.lizzandro@gmail.com>
2026-05-25 14:51:46 -0300

    Atualização de rotas

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    return Inertia::render('SistemaCadastro');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/SistemaCadastro', function () {
